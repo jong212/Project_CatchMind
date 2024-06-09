65238b9c312121a6dab9049bcc76287c95dd1aea
jonghwa <jonghwa0212@gmail.com>
2024-06-09 16:29:16 +0900

    Ui작업중

---
+++ Assets/Cartoon Mobile UI Pack/scripts/ButtonAnimator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+
+public class ButtonAnimator : MonoBehaviour, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private Animator buttonAnimator;
+    // Use this for initialization
+    void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonAnimator.SetTrigger("Normal");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonAnimator.SetTrigger("Pressed");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonAnimator.SetTrigger("Normal");
+    }
+}
